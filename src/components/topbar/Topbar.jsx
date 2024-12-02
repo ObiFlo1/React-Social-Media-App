c9404a9ee0d27c8ed27330da8e79b255d1222ae4
ObiFlo1 <rubflores1000@gmail.com>
2024-12-02 04:22:38 -0800

    topbar finished

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,48 @@
+import "./topbar.css";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonIcon from "@mui/icons-material/Person";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+
+function topbar() {
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="logo">SocialClone</span>
+      </div>
+
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <SearchIcon className="searchIcon" />
+          <input
+            placeholder="Search for friend, post or video"
+            className="searchInput"
+          />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLinks">Homepage</span>
+          <span className="topbarLinks">TimeLine</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <PersonIcon />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <ChatIcon />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <NotificationsIcon />
+            <span className="topbarIconBadge">1</span>
+          </div>
+        </div>
+        <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+      </div>
+    </div>
+  );
+}
+
+export default topbar;
